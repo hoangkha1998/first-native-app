791e177649b0249ecec1458a66da55c421c51fa6
nguyen_kha <nguyen_kha@kozo-japan.com>
2021-02-04 10:47:39 +0700

    create components

---
+++ components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+
+export default function FooterScreen(){
+    return(
+        <View>
+            <Text>
+                this is Footer
+            </Text>
+        </View>
+    )
+}
